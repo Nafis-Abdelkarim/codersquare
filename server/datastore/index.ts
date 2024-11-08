362a9572703ecd722ef4bf2cd8d9d540186847bb
Nafis-Abdelkarim <kazcrb@gmail.com>
2024-11-08 17:27:37 +0100

    setup sqlite

--- server/datastore/index.ts
+++ server/datastore/index.ts
@@ -3,8 +3,16 @@ import { LikeDao } from "./dao/LikeDao";
 import { InMemoryDatastore } from "./memorydb";
 import { PostDao } from "./dao/PostDao";
 import { UserDao } from "./dao/UserDao";
+import { SqlDataStore } from "./sql";
 
+// Define the Datastore interface
+export interface Datastore extends UserDao, PostDao, LikeDao, CommentDao {}
 
-export interface Datastore extends UserDao, PostDao, LikeDao, CommentDao {};
+// Export `db` so it can be used in other modules
+export let db: Datastore;
 
-export const db = new InMemoryDatastore();
+export async function initDb() {
+    // Initialize `db` with the chosen datastore implementation
+    // db = new InMemoryDatastore();
+    db = await new SqlDataStore().openDb();
+}
